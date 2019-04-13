d54e714d8afef9977bb551d91fa9dc8aae15d190
Stephanie <stephaniesantamaria58@yahoo.com>
2019-04-12 22:25:43 -0700

    saving assignment, not complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react';
+import * as firebase from 'firebase';
 import logo from './logo.svg';
 import './App.css';
+
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCNTe6vOmrhQXa8JJm8ldVKhMwD6Kb29sQ",
+    authDomain: "bloc-chat-23rc.firebaseapp.com",
+    databaseURL: "https://bloc-chat-23rc.firebaseio.com",
+    projectId: "bloc-chat-23rc",
+    storageBucket: "bloc-chat-23rc.appspot.com",
+    messagingSenderId: "905975327068"
+  };
+  firebase.initializeApp(config);
 
 class App extends Component {
   render() {
@@ -8,9 +21,6 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
           <a
             className="App-link"
             href="https://reactjs.org"
